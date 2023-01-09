ecd0e5a3ba79cf05b9b39860432175b1d5fecdd1
NikolayDvoinikh <nikolaydvoinikh@gmail.com>
2023-01-09 18:27:58 +0200

    start second

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,27 +1,34 @@
-{
-  /* <section class="statistics">
-  <h2 class="title">Upload stats</h2>
+import PropTypes from 'prop-types';
+import css from './statistics.module.css';
 
-  <ul class="stat-list">
-    <li class="item">
-      <span class="label">.docx</span>
-      <span class="percentage">4%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp3</span>
-      <span class="percentage">14%</span>
-    </li>
-    <li class="item">
-      <span class="label">.pdf</span>
-      <span class="percentage">41%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp4</span>
-      <span class="percentage">12%</span>
-    </li>
-  </ul>
-</section>; */
+export default function Title({ title }) {
+  return <h2 className={css.title}>{title}</h2>;
 }
+
+// {
+//   /* <section class="statistics">
+//   <h2 class="title">Upload stats</h2>
+
+//   <ul class="stat-list">
+//     <li class="item">
+//       <span class="label">.docx</span>
+//       <span class="percentage">4%</span>
+//     </li>
+//     <li class="item">
+//       <span class="label">.mp3</span>
+//       <span class="percentage">14%</span>
+//     </li>
+//     <li class="item">
+//       <span class="label">.pdf</span>
+//       <span class="percentage">41%</span>
+//     </li>
+//     <li class="item">
+//       <span class="label">.mp4</span>
+//       <span class="percentage">12%</span>
+//     </li>
+//   </ul>
+// </section>; */
+// }
 
 // Компонент должен принимать два пропа title и stats, в которых указывается заголовок и объект статистики.
 
